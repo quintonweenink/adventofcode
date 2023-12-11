dbc38c2ae97222aa8dd7e444c2333a6fa2fd9398
quinton.weenink <qweenink@gmail.com>
2023-12-11 21:29:49 +0100

    day 11 puzzel 1 & 2

---
+++ src/test/kotlin/nl/qweenink/adventofcode2023/Day12.kt
@@ -0,0 +1,21 @@
+package nl.qweenink.adventofcode2023
+
+import org.junit.jupiter.api.Test
+import java.lang.RuntimeException
+import kotlin.math.abs
+
+class Day12 {
+
+    @Test
+    fun puzzle1() {
+        val lines = data.split(System.getProperty("line.separator"))
+    }
+
+    @Test
+    fun puzzle2() {
+    }
+
+    val data2 = """"""
+
+    val data = """"""
+}
